6c1fc5a990c7d33f374f074c8ab977b35014c6c2
shreyash  Dholakia <shreyashdholakia@gmail.com>
2015-10-30 16:04:42 -0500

    teams page  added player statistics

--- app/scripts/controllers/profileCtrl.js
+++ app/scripts/controllers/profileCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('howWasThat')
-  .controller('ProfileCtrl', function ($scope, ProfileService, $location, $routeParams, $rootScope, $http, $cookieStore, matchDetailsService, alertService) {
+  .controller('ProfileCtrl', function ($scope, ProfileService, $location, $routeParams, $rootScope, $http, $cookieStore, matchDetailsService, alertService, statisticService) {
 
     $scope.isProfileCreated = false;
     $scope.currentPage = 0;
@@ -9,19 +9,21 @@ angular.module('howWasThat')
     $scope.userProfile = false;
     $scope.email = $rootScope.currentUser.email;
 
-    $scope.checkProfileCreated = function () {
+    function checkProfileCreated() {
       ProfileService.findProfile($rootScope.currentUser.username).success(function (response) {
         $scope.profileExists = response.exists;
         $scope.userProfile = true;
         $scope.user = response.data;
         $scope.fullName = $scope.user.firstname + ' ' + $scope.user.lastname;
         getMatches($scope.fullName);
+        getUserStatistics($scope.fullName);
+        getUserTeams($scope.user.email);
       }).error(function (status, data) {
         alertService.displayErrorMessage("There was an error! Please try again.");
       });
-    };
+    }
 
-    $scope.checkProfileCreated();
+    checkProfileCreated();
 
     function getMatches(fullName) {
       matchDetailsService.getUserMatches(fullName).success(function (response) {
@@ -51,7 +53,6 @@ angular.module('howWasThat')
       user.username = $rootScope.currentUser.username;
       user.email = $scope.email;
       user.joiningDate = new Date();
-
       ProfileService.create(user).success(function (data) {
         $scope.userProfile = true;
         alertService.displaySaveMessage("Profile Successfully created");
@@ -72,6 +73,41 @@ angular.module('howWasThat')
     $scope.isActiveTab = function (tab) {
       return $scope.tab === tab;
     };
+
+    function getUserStatistics(name) {
+      statisticService.userStatistics(name).success(function (response) {
+        $scope.stats = response.data;
+        extractUserStatistics($scope.stats, name);
+      }).error(function (status, data) {
+        alertService.displayErrorMessage("There was an error! Please try again.");
+      });
+    }
+
+    function extractUserStatistics(stats, name) {
+      var statistics = [];
+      for (var i in stats) {
+        var runs = _.where(stats[i].teams, {player: name});
+        statistics.push(runs[0]);
+      }
+      $scope.playerRuns = 0;
+      $scope.playerWickets = 0;
+      $scope.playerGames = 0;
+
+      _.each(statistics, function (item) {
+        $scope.playerRuns += Number(item['runs']);
+        $scope.playerWickets += Number(item['wickets']);
+        $scope.playerGames += Number(item['matches']);
+      });
+    }
+
+    function getUserTeams(name) {
+      statisticService.userTeams(name).success(function (response) {
+        $scope.playerTeams = response.data;
+        console.log($scope.playerTeams);
+      }).error(function (status, data) {
+        alertService.displayErrorMessage("There was an error! Please try again.");
+      });
+    }
 
   });
 
