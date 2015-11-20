28e967b57bd91a983d3e01c45556b98941281597
shreyashdholakia <shreyashdholakia@gmail.com>
2015-11-19 21:52:23 -0600

    profile pic update

--- app/scripts/controllers/profileCtrl.js
+++ app/scripts/controllers/profileCtrl.js
@@ -16,7 +16,6 @@ angular.module('howWasThat')
         $scope.profileExists = response.exists;
         $scope.userProfile = true;
         $scope.user = response.data;
-        console.log($scope.user);
         $scope.fullName = $scope.user.firstname + ' ' + $scope.user.lastname;
         $scope.email = $rootScope.currentUser.email;
         $scope.image = response.image;
@@ -52,18 +51,23 @@ angular.module('howWasThat')
     }
 
     $scope.updateProfile = function (user) {
-      if ($scope.profileExists) {
-        user.username = $rootScope.currentUser.username;
-        user.updatedDate = new Date();
-        ProfileService.update(user).success(function (data) {
-          $scope.userProfile = true;
-          alertService.displaySaveMessage("Profile Successfully updated");
-          $location.path("/profile");
-        }).error(function (status, data) {
-          alertService.displayErrorMessage("There was an error! Please try again.");
-        });
+      if(uploader.queue.length === 1) {
+        alertService.displayLoadingMessage('Adding attachment...');
+        uploader.uploadAll();
       } else {
-        $scope.create(user);
+        if ($scope.profileExists) {
+          user.username = $rootScope.currentUser.username;
+          user.updatedDate = new Date();
+          ProfileService.update(user).success(function (data) {
+            $scope.userProfile = true;
+            alertService.displaySaveMessage("Profile Successfully updated");
+            $location.path("/profile");
+          }).error(function (status, data) {
+            alertService.displayErrorMessage("There was an error! Please try again.");
+          });
+        } else {
+          $scope.create(user);
+        }
       }
     };
 
@@ -159,11 +163,10 @@ angular.module('howWasThat')
       if(response.message) {
         alertService.displayErrorMessage(response.message);
       } else {
-
-        $scope.image = '<img src="data:image/png;base64,' + $.base64.encode(response.data.avatar.data) + '" />';
-        console.log($scope.image);
         alertService.displaySaveMessage("Profile Successfully created");
+        $("#upload-file-info").value = null;
         getProfile();
+
       }
     });
 
